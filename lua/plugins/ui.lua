e8fe7658fc29d66b4bfee9cf4d6bee71f6616f1d
luxus <luxuspur@gmail.com>
2023-08-04 15:48:06 +0200

    prefer ui instead of colorscheme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,8 @@
 return {
   "AstroNvim/astroui",
+  ---@type AstroUIOpts
   opts = {
+    status = { separators = { breadcrumbs = "  ", path = "/" } },
     colorscheme = "astrodark", -- change colorscheme
     highlights = {
       init = { -- this table overrides highlights in all themes
